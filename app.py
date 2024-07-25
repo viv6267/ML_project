c9a00eb72852744af41c299f2e0f96a69e68f6a7
Vivek_rag_gemini <viv6267@gmail.com>
2024-07-25 10:37:01 +0530

    Model training is done and selected best model via hypertuning techniques

--- app.py
+++ app.py
@@ -3,18 +3,28 @@ from src.machine_learning_ops.exception import CustomException
 import sys
 from src.machine_learning_ops.components.data_ingestion import DataIngestion
 from src.machine_learning_ops.components.data_transformation import DataTransformation
+from src.machine_learning_ops.components.model_trainer import ModelTrainer
 
 if __name__=='__main__':
     logging.info("The execution has started.")
 
     try:
+        # data injection
         data_injestion = DataIngestion()
         train_data_path,test_data_path=data_injestion.initiate_data_ingestion()
         # logging.info("Data Ingestion Process Completed Successfully.")
 
+
+        # data transformation
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
         logging.info("Data Transformation Process Completed Successfully.")
+
+        # model trainer
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        logging.info("Model Training Process Completed Successfully and model pickle file saved.")
+
         
     except Exception as e:
         logging.INFO("Custom Exception")
